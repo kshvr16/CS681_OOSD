c501eab4743f05d5edd73277be5ed23da47cdef1
kshvr16 <101598122+kshvr16@users.noreply.github.com>
2022-10-30 19:58:05 -0400

    HomeWork 07

---
+++ HW07/src/edu/umb/cs681/primes/RunnablePrimeGenerator.java
@@ -0,0 +1,13 @@
+package edu.umb.cs681.primes;
+
+public class RunnablePrimeGenerator extends PrimeGenerator implements Runnable {
+
+    public RunnablePrimeGenerator(long from, long to) {
+        super(from, to);
+    }
+
+    public void run() {
+        generatePrimes();
+    }
+
+}
